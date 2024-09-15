6625d7199963bf35e04212b0eb952da108d954fc
JaisonRoy <jaisonroy700@gmail.com>
2024-09-16 00:02:30 +0530

    home and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
-import './App.css'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Pages/Home";
+import Contact from "./Components/Pages/Contact";
 
 function App() {
- 
-
   return (
     <>
-    <BrowserRouter>
-    <Routes>
-      {/* <Route path='/' element={}/> */}
-       </Routes>
-    </BrowserRouter>
-    
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/products" element={<Products />} />
+          <Route path="/abotus" element={<Aboutus />} /> */}
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
