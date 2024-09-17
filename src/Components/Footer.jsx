d1bc4392c81602b7167e22b099348dbf9a6f923b
KAVYA\kavya <kavyakb23@gmail.com>
2024-09-17 15:47:00 +0530

    edited

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,42 +3,52 @@ import { Container, Row, Col } from 'react-bootstrap';
 import './Styles/footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faHouse, faPhone } from '@fortawesome/free-solid-svg-icons';
+
+
 
 
 function Footer() {
   return (
-    <footer className="text-secondary pt-4">
+    <footer className=" pt-4">
       <Container>
         <Row>
-          <Col md={4}>
-            <img  className='logoImg'src="https://keralakaumudi.com/web-news/en/2023/11/NMAN0449546/image/kudumbashree.1699496950.jpg" alt="logo" />
+          <Col md={3}>
+            <img className='logoImg' src="https://keralakaumudi.com/web-news/en/2023/11/NMAN0449546/image/kudumbashree.1699496950.jpg" alt="logo" />
             <p>
               We are a team committed to delivering quality content and
               services to our customers. Our mission is to create the best
               user experience.
             </p>
           </Col>
-          <Col md={4}>
+          <Col md={3}>
             <h5>Quick Links</h5>
             <ul className="list-unstyled">
-              <li><a href="/"  className="text-secondary">Home</a></li>
-              <li><a href="/about" className="text-secondary">About Us</a></li>
-              <li><a href="/gallery"  className="text-secondary">Gallery</a></li>
-              <li><a href="/contact" className="text-secondary">Contact Us</a></li>
+              <li><a href="/"  >Home</a></li>
+              <li><a href="/about">About Us</a></li>
+              <li><a href="/gallery" >Gallery</a></li>
+              <li><a href="/contact">Contact Us</a></li>
             </ul>
           </Col>
-          <Col md={4}>
+          <Col md={3}>
+            <h5>Contact us</h5>
+             <p> <FontAwesomeIcon icon={faHouse} />kudumbashree office tvm</p>
+             <p><FontAwesomeIcon icon={faEnvelope} />k@gmail.com</p>
+             <p><FontAwesomeIcon icon={faPhone} />+91**********</p>
+          </Col>
+          <Col md={3}>
             <h5>Follow Us</h5>
             <ul className="list-unstyled d-flex">
-              <li><a href="#"  className="text-secondary me-3"><FontAwesomeIcon icon={faFacebook} /></a></li>
-              <li><a href="#" className="text-secondary me-3"><FontAwesomeIcon icon={faTwitter} /></a></li>
-              <li><a href="#" className="text-secondary me-3"><FontAwesomeIcon icon={faInstagram} /></a></li>
-              <li><a href="#"className="text-secondary me-3"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+              <li><a href="#" className=" me-3"><FontAwesomeIcon icon={faFacebook} /></a></li>
+              <li><a href="#" className=" me-3"><FontAwesomeIcon icon={faTwitter} /></a></li>
+              <li><a href="#" className=" me-3"><FontAwesomeIcon icon={faInstagram} /></a></li>
+              <li><a href="#" className="me-3"><FontAwesomeIcon icon={faLinkedin} /></a></li>
             </ul>
           </Col>
+
         </Row>
         <Row className="mt-4">
-          <Col className="bg-bg-body-tertiary text-center">
+          <Col className="text-center">
             <p>&copy; {new Date().getFullYear()} Your Website. All rights reserved.</p>
           </Col>
         </Row>
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,35 +1,71 @@
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import './Styles/header.css'
+import './Styles/header.css';
 
 function OffcanvasExample() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  // Update window width on resize
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener('resize', handleResize);
+    
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <>
-      <Navbar key={1} expand={false} className="bg-body-tertiary mb-3">
+    <div className='header'>
+      <Navbar expand="lg" className="mb-3">
         <Container fluid>
-          <Navbar.Brand href="#"><img className='logo' src="https://keralakaumudi.com/web-news/en/2023/11/NMAN0449546/image/kudumbashree.1699496950.jpg" alt="" /></Navbar.Brand>
-          <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${false}`} />
-          <Navbar.Offcanvas
-            id={`offcanvasNavbar-expand-${false}`}
-            aria-labelledby={`offcanvasNavbarLabel-expand-${false}`}
-            placement="end"
-          >
-            <Offcanvas.Header closeButton className='close'>
-            </Offcanvas.Header>
-            <Offcanvas.Body className='background'>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <Nav.Link className='icons' href="/">HOME</Nav.Link>
-                <Nav.Link className='icons' href="/about">ABOUT US</Nav.Link>
-                <Nav.Link className='icons' href="/gallery">GALLERY</Nav.Link>
-                <Nav.Link className='icons' href="/contact">CONTACT US</Nav.Link>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
+          <Navbar.Brand href="#">
+            <img className='logo' src="https://keralakaumudi.com/web-news/en/2023/11/NMAN0449546/image/kudumbashree.1699496950.jpg" alt="logo" />
+          </Navbar.Brand>
+
+          {/* Conditionally render Navbar.Toggle for small screens or inline Nav for larger screens */}
+          {windowWidth < 992 ? (
+            <>
+              {/* Wrapper for positioning the toggle button */}
+              <div className="toggle-wrapper">
+                <Navbar.Toggle aria-controls="offcanvasNavbar" />
+              </div>
+
+              {/* Offcanvas menu */}
+              <Navbar.Offcanvas
+                id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel"
+                placement="end"  // Ensure it opens on the right
+                className="offcanvas-menu"  // For additional custom styles
+              >
+                <Offcanvas.Header closeButton className='close'>
+                </Offcanvas.Header>
+                <Offcanvas.Body className='background'>
+                  <Nav className="justify-content-end flex-grow-1 pe-3">
+                    <Nav.Link className='icons' href="/">HOME</Nav.Link>
+                    <Nav.Link className='icons' href="/about">ABOUT US</Nav.Link>
+                    <Nav.Link className='icons' href="/gallery">GALLERY</Nav.Link>
+                    <Nav.Link className='icons' href="/contact">CONTACT US</Nav.Link>
+                  </Nav>
+                </Offcanvas.Body>
+              </Navbar.Offcanvas>
+            </>
+          ) : (
+            /* Inline Navbar links on large screens */
+            <Nav className="d-flex">
+              <Nav.Link className='icons-lg' href="/">HOME</Nav.Link>
+              <Nav.Link className='icons-lg' href="/about">ABOUT US</Nav.Link>
+              <Nav.Link className='icons-lg' href="/gallery">GALLERY</Nav.Link>
+              <Nav.Link className='icons-lg' href="/contact">CONTACT US</Nav.Link>
+            </Nav>
+          )}
         </Container>
       </Navbar>
-    </>
+    </div>
   );
 }
 
